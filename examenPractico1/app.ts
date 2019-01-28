b765c4c4f723e6db8dfc5ccc292559e6bda2d0f0
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-01-28 12:25:47 +0100

    Primer examen practico realizado

--- examenPractico1/app.ts
+++ examenPractico1/app.ts
@@ -1,16 +1,20 @@
 
 // Uso de Let y Const
-var nombre = "Ricardo Tapia";
-var edad = 23;
+let nombre = "Ricardo Tapia";
+let edad = 23;
 
-var PERSONAJE = {
+const PERSONAJE = {
   nombre: nombre,
   edad: edad
 };
 
 
 // Cree una interfaz que sirva para validar el siguiente objeto
-var batman = {
+interface Batman {
+  nombre:string,
+  artesMarciales:string[],
+}
+let batman:Batman = {
   nombre: "Bruno Díaz",
   artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
 }
@@ -20,12 +24,14 @@ function resultadoDoble( a, b ){
   return (a + b) * 2
 }
 
+let resultadoDobleF = (a:number, b:number) => (a+b*2);
+
 // Función con parametros obligatorios, opcionales y por defecto
 // donde NOMBRE = obligatorio
 //       PODER  = opcional
 //       ARMA   = por defecto = "arco"
-function getAvenger( nombre, poder, arma ){
-  var mensaje;
+function getAvenger( nombre:string, poder?:string, arma:string = "arco" ){
+  let mensaje;
   if( poder ){
      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
   }else{
@@ -40,3 +46,10 @@ function getAvenger( nombre, poder, arma ){
 //   * altura
 // También un método que calcule el área  =  base * altura,
 // ese método debe de retornar un numero.
+
+class Rectangulo {
+  public base:number;
+  public altura:number;
+
+  get area(): number {return this.base * this.altura;};
+}
